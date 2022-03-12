b5a7ee26a416b938593d3ed7f969eb98f2e5126b
Chris Zheng <z@caudate.me>
2022-03-13 00:13:37 +0800

    SRC:cec3959b42f465f47ce5c27f323f0ae7686b3e07

--- src/pthreads_hello.c
+++ src/pthreads_hello.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <pthread.h>
 
 void * print_message_function(void * ptr){
@@ -12,8 +14,8 @@ main(){
   char * message1 = "Thread 1";
   char * message2 = "Thread 2";
   int iret1, iret2;
-  iret1=pthread_create(&thread1,NULL,print_message_function,((void *)message1));
-  iret2=pthread_create(&thread2,NULL,print_message_function,((void *)message2));
+  iret1 = pthread_create(&thread1,NULL,print_message_function,((void *)message1));
+  iret2 = pthread_create(&thread2,NULL,print_message_function,((void *)message2));
   pthread_join(thread1,NULL);
   pthread_join(thread2,NULL);
   printf("Thread 1 returns: %d\n",iret1);
